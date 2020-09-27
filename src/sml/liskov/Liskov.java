33433a192f863fcc64a8b18125bc67477137f2ae
sunmaolin <Mr.sun727@iCloud.com>
2020-09-27 23:01:39 +0800

    里氏替换原则 错误实例

---
+++ src/sml/liskov/Liskov.java
@@ -0,0 +1,36 @@
+package sml.liskov;
+
+public class Liskov {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println("11-3="+a.func1(11,3));
+        System.out.println("1-8="+a.func1(1,8));
+
+        B b = new B();
+        //这里本意是求出11-3
+        System.out.println("11-3="+b.func1(11,3));
+        //本意求出1-8
+        System.out.println("1-8="+b.func1(1,8));
+        System.out.println("11+3+9="+b.func2(11,3));
+    }
+}
+
+//A类
+class A {
+    //返回俩个数的差
+    public int func1(int num1,int num2){
+        return num1 - num2;
+    }
+}
+
+
+class B extends A {
+    @Override
+    public int func1(int num1, int num2) {
+        return num1+num2;
+    }
+
+    public int func2(int num1,int num2){
+        return func1(num1,num2)+9;
+    }
+}
