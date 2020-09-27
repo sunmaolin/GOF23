daf06e5d43f0809fb032d1a348048ce2efa040af
sunmaolin <Mr.sun727@iCloud.com>
2020-09-27 22:00:23 +0800

    依赖倒转原则 正确实例

---
+++ src/sml/inversion/improve/DependecyInversion.java
@@ -0,0 +1,35 @@
+package sml.inversion.improve;
+
+public class DependecyInversion {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.receive(new Email());
+        person.receive(new WeiXin());
+    }
+}
+
+//定义一个接口
+interface IReceiver {
+    public String getInfo();
+}
+
+class Email implements IReceiver {
+    @Override
+    public String getInfo() {
+        return "电子邮件信息：Hello world";
+    }
+}
+
+class WeiXin implements IReceiver {
+    @Override
+    public String getInfo() {
+        return "微信信息：Hello OK";
+    }
+}
+
+//方式2
+class Person {
+    public void receive(IReceiver iReceiver) {
+        System.out.println(iReceiver.getInfo());
+    }
+}
