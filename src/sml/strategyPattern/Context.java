bcf91b23e008b2ebaefa7fabdf5145dc201b6bac
sunmaolin <Mr.sun727@iCloud.com>
2020-11-22 22:07:43 +0800

    菜鸟教程  策略模式

---
+++ src/sml/strategyPattern/Context.java
@@ -0,0 +1,13 @@
+package sml.strategyPattern;
+
+public class Context {
+    private Strategy strategy;
+
+    public Context(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public int executeStrategy(int num1,int num2){
+        return this.strategy.doOperation(num1,num2);
+    }
+}
---
+++ src/sml/strategyPattern/Strategy.java
@@ -0,0 +1,7 @@
+package sml.strategyPattern;
+
+public interface Strategy {
+
+    int doOperation(int num1,int num2);
+
+}
