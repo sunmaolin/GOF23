1306f060f0488f2bc26cdc96a14d40bf1a61b346
sunmaolin <Mr.sun727@iCloud.com>
2020-11-22 21:44:45 +0800

    菜鸟教程  状态模式

---
+++ src/sml/statePattern/StopState.java
@@ -0,0 +1,12 @@
+package sml.statePattern;
+
+public class StopState implements State {
+    public void doAction(Context context) {
+        System.out.println("Player is in stop state");
+        context.setState(this);
+    }
+
+    public String toString(){
+        return "Stop State";
+    }
+}
