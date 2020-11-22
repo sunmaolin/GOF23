4695de4761ba8e1a726c9114fc7821af907e105f
sunmaolin <Mr.sun727@iCloud.com>
2020-11-22 22:13:50 +0800

    菜鸟教程  模板模式

---
+++ src/sml/templatePattern/templatePatternDemo.java
@@ -0,0 +1,11 @@
+package sml.templatePattern;
+
+public class templatePatternDemo {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
